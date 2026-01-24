4838b8021987c103deaa9e32d5606a9fcca9194a
DanyloKoval <danylo.koval@unit8.co>
2026-01-24 15:24:58 +0100

    Initial version Hero Slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import HeroSlider from "@/components/HeroSlider"
+
+const slides = [
+  {
+    id: 1,
+    src: "/images/slide3.png",
+    alt: "Уютный вязаный свитер",
+    title: "Warmth You Can Feel",
+    subtitle: "HANDMADE WITH LOVE"
+  },
+  {
+    id: 2,
+    src: "/images/slide2.png",
+    alt: "Уютный вязаный свитер",
+  }
+]
+
 export default function Home() {
   return (
     <>
-      Main Page
+      <HeroSlider slides={slides} />
     </>
   )
 }
